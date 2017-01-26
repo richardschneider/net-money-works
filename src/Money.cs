eaa6524f22a85cb5755ef2493596d012cd3d1e74
Richard Schneider <makaretu@gmail.com>
2017-01-26 21:17:11 +1300

    feat(Money): creating Money

---
+++ src/Money.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace MoneyWorks
+{
+    /// <summary>
+    ///   Represents a monetary value and currency code.
+    /// </summary>
+    public class Money
+    {
+        static Regex iso4217 = new Regex(@"[A-Z]{3}", RegexOptions.Compiled | RegexOptions.CultureInvariant);
+
+        /// <summary>
+        ///   Creates a new instance of the <see cref="Money"/> class
+        ///   with the specified <see cref="Amount"/> and
+        ///   <see cref="Currency"/>.
+        /// </summary>
+        /// <param name="amount">The amount of money.</param>
+        /// <param name="currency">
+        ///   An <see href="https://en.wikipedia.org/wiki/ISO_4217">ISO 4217</see> currency code.  For example: "CNY", "NZD" or "JPY".
+        /// </param>
+        public Money(decimal amount, string currency)
+        {
+            if (currency.Length != 3 || !iso4217.IsMatch(currency))
+                throw new FormatException($"'{currency}' is not a valid ISO 4217 currency code.");
+
+            Amount = amount;
+            Currency = currency;
+        }
+
+        /// <summary>
+        ///   The amount of money.
+        /// </summary>
+        public decimal Amount { get; }
+
+        /// <summary>
+        ///   The currency of the money.
+        /// </summary>
+        /// <value>
+        ///   An <see href="https://en.wikipedia.org/wiki/ISO_4217">ISO 4217</see> currency code.  For example: "CNY", "NZD" or "JPY".
+        /// </value>
+        public string Currency { get; }
+    }
+}
---
+++ test/MoneyWorksTest/Constructor.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MoneyWorks
+{
+    [TestClass]
+    public class ConstructorTest
+    {
+        [TestMethod]
+        public void Accepts_DecimalAmount()
+        {
+            var money = new Money(123.45m, "JPY");
+            Assert.AreEqual(123.45m, money.Amount);
+            Assert.AreEqual("JPY", money.Currency);
+
+            var min = new Money(Decimal.MinValue, "JPY");
+            Assert.AreEqual(Decimal.MinValue, min.Amount);
+            Assert.AreEqual("JPY", min.Currency);
+
+            var max = new Money(Decimal.MaxValue, "JPY");
+            Assert.AreEqual(Decimal.MaxValue, max.Amount);
+            Assert.AreEqual("JPY", min.Currency);
+        }
+
+        [TestMethod]
+        public void Accepts_Int32Amount()
+        {
+            var money = new Money(123, "JPY");
+            Assert.AreEqual(123, money.Amount);
+            Assert.AreEqual("JPY", money.Currency);
+
+            var min = new Money(Int32.MinValue, "JPY");
+            Assert.AreEqual(Int32.MinValue, min.Amount);
+            Assert.AreEqual("JPY", min.Currency);
+
+            var max = new Money(Int32.MaxValue, "JPY");
+            Assert.AreEqual(Int32.MaxValue, max.Amount);
+            Assert.AreEqual("JPY", min.Currency);
+        }
+
+        [TestMethod]
+        public void Accepts_Int64Amount()
+        {
+            var money = new Money(123L, "JPY");
+            Assert.AreEqual(123, money.Amount);
+            Assert.AreEqual("JPY", money.Currency);
+
+            var min = new Money(Int64.MinValue, "JPY");
+            Assert.AreEqual(Int64.MinValue, min.Amount);
+            Assert.AreEqual("JPY", min.Currency);
+
+            var max = new Money(Int64.MaxValue, "JPY");
+            Assert.AreEqual(Int64.MaxValue, max.Amount);
+            Assert.AreEqual("JPY", min.Currency);
+        }
+
+        [TestMethod]
+        public void Throws_On_Invalid_Currency()
+        {
+            var ok = new Money(0, "NZD");
+
+            ExceptionAssert.Throws<FormatException>(() => new Money(0, "N"));
+            ExceptionAssert.Throws<FormatException>(() => new Money(0, "NZ"));
+            ExceptionAssert.Throws<FormatException>(() => new Money(0, "NZDX"));
+            ExceptionAssert.Throws<FormatException>(() => new Money(0, "nzd"));
+        }
+    }
+}
